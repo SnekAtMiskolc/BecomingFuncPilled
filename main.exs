a97b220618c60466fe52c0bc23bd8a496a77e018
Susa Milán <mailtosnek@protonmail.com>
2023-02-19 09:53:26 +0100

    Goofy functions

--- main.exs
+++ main.exs
@@ -14,13 +14,25 @@ defmodule User do
     IO.puts "Created new user with name: " <> name
     %User{name: name, age: age}
   end
+  @spec create(String.t, String.t, String.t, String.t) :: [%User{}]
+  def create(name, age, name2, age2) do
+    IO.puts "Created new user with name: " <> name
+    [%User{name: name, age: age}, %User{name: name2, age: age2}]
+  end
 
   def put(user = %User{}) do
     IO.puts "Username: " <> user.name
     IO.puts "User age: #{user.age}"
   end
+  @spec put(list(%User{})) :: :ok
+  def put(users) do
+    Enum.each(users, fn(u) -> User.put(u) end)
+    :ok
+  end
 
 end
 
 user = User.create("John Doe", 24)
+users = User.create("John Doe", 24, "Peter Griffin", 45)
+users |> User.put
 user |> User.put
