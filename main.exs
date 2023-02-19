9a044a4198e177c20c5631338c95da147814b9fa
Susa Milán <mailtosnek@protonmail.com>
2023-02-19 08:06:40 +0100

    Some struct stuff

--- main.exs
+++ main.exs
@@ -1,7 +1,25 @@
 defmodule Say do
-  def hello_world do
-    IO.puts "Hello, World!"
+  def hello_world(name) do
+    IO.puts "Hello " <> name <> "!"
   end
 end
 
-Say.hello_world()
+Say.hello_world("John")
+
+defmodule User do
+  defstruct name: "Jonny", age: 2333
+
+  @spec create(String.t, String.t) :: %User{}
+  def create(name, age) do
+    IO.puts "Created new user with name: " <> name
+    %User{name: name, age: age}
+  end
+
+  def put(user = %User{}) do
+    IO.puts "Username: " <> user.name
+    IO.puts "User age: #{user.age}"
+  end
+
+end
+
+User.create("John Doe", 24) |> User.put
