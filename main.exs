4e444fd70a9f9a9602e6c60060506b12678c552a
Susa Milán <mailtosnek@protonmail.com>
2023-02-19 16:49:07 +0100

    Testing with magit

--- main.exs
+++ main.exs
@@ -1,6 +1,6 @@
 defmodule Say do
   def hello_world(name) do
-    IO.puts "Hello " <> name <> "!"
+    IO.puts("Hello " <> name <> "!")
   end
 end
 
@@ -9,30 +9,38 @@ Say.hello_world("John")
 defmodule User do
   defstruct name: "", age: 0
 
-  @spec create(String.t, String.t) :: %User{}
+  @spec create(String.t(), String.t()) :: %User{}
   def create(name, age) do
-    IO.puts "Created new user with name: " <> name
+    IO.puts("Created new user with name: " <> name)
     %User{name: name, age: age}
   end
-  @spec create(String.t, String.t, String.t, String.t) :: [%User{}]
+
+  @spec create(String.t(), String.t(), String.t(), String.t()) :: [%User{}]
   def create(name, age, name2, age2) do
-    IO.puts "Created new user with name: " <> name
+    IO.puts("Created new user with name: " <> name)
     [%User{name: name, age: age}, %User{name: name2, age: age2}]
   end
 
   def put(user = %User{}) do
-    IO.puts "Username: " <> user.name
-    IO.puts "User age: #{user.age}"
-  end
-  @spec put(list(%User{})) :: :ok
-  def put(users) do
-    Enum.each(users, fn(u) -> User.put(u) end)
-    :ok
+    IO.puts("Username: " <> user.name)
+    IO.puts("User age: #{user.age}")
   end
 
+  @spec put(list(%User{})) :: :ok
+  def put(users) do
+    Enum.each(users, fn u -> User.put(u) end)
+    :ok
+  end
 end
 
 users = User.create("John Doe", 24, "Peter Griffin", 45)
 user = [User.create("John Doe", 24)]
 users = users ++ user
-users |> User.put
+users |> User.put()
+
+# These two lambda function declarations are the same.
+# & <- Capture operator
+# lambda = fn x, y, z -> z * y + x end
+lambda = &(&1 + &2 + 2)
+
+lambda.(1, 2) |> IO.puts()
